690949cfea59e8faf41a6ba1053574b01a686411
elisegordonn <114438680+elisegordonn@users.noreply.github.com>
2024-03-05 16:32:02 -0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import collegeData from './CollegeBasketballTeams.json';
 
-function App() {
+interface CollegeProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+const collegeNames = collegeData.teams;
+
+function Welcome() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <p>Hello</p>
-      </header>
+    <div>
+      <h1>NCAA Basketball Colleges</h1>
+      <h3>
+        With March Madness upon us, we have created a website to list out
+        information about all the colleges involved in NCAA Basketball.
+      </h3>
     </div>
   );
 }
+class College extends React.Component<CollegeProps> {
+  render() {
+    const oneCollege = this.props;
+    return (
+      <div>
+        <h2>School: {oneCollege.school}</h2>
+        <h3>Mascot: {oneCollege.name}</h3>
+        <h3>
+          Location: {oneCollege.city}, {oneCollege.state}
+        </h3>
+      </div>
+    );
+  }
+}
 
+function CollegeList() {
+  return (
+    <div>
+      {collegeNames.map((oneCollege) => (
+        <College {...oneCollege} />
+      ))}
+    </div>
+  );
+}
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <CollegeList />
+    </div>
+  );
+}
 export default App;
